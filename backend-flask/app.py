e8ae4d3ff20a6f006b3ba930ae7af08ddf081f81
sasthikas24 <sasthikas24@gmail.com>
2026-04-05 20:49:15 +0530

    Added gunicorn to requirements

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -181,5 +181,5 @@ def create_ticket():
 # -------------------- RUN APP --------------------
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
